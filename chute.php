05ba422928943d4568eb00695ea74a579c83a4e3
MATI4S0 <mateusfrancapaula@gmail.com>
2025-10-31 15:15:45 -0300

    Enhance game logic with points and lives tracking

--- chute.php
+++ chute.php
@@ -2,19 +2,38 @@
 
     $valorCorreto = random_int(1, 10);
 
+    $pontos = 10;
+
+    $vidas = 3;
+
+    print("Você possui: $vidas vidas\n\n");
+
     do{
       
         $chute = (int) readline("Digite um valor: ");
 
         if($chute == $valorCorreto)
         {
-            print("Você acertou o valor\n");
+            print("\nVocê acertou o valor!!\n");
+            $pontos = $pontos + 15;
+
         }elseif ($chute > $valorCorreto)
         {
-            print("Você chutou um numero mais alto que o valor correto\n");
+            print("Você chutou um numero mais alto que o valor correto\n\n");
+            $vidas = $vidas - 1;
+            print("Você possui: $vidas vidas\n");
+            $pontos = $pontos - 3;
         }else{
-            print("Você chutou um numero mais baixo que o valor correto\n");
+            print("Você chutou um numero mais baixo que o valor correto\n\n");
+            $vidas = $vidas - 1;
+            $pontos = $pontos - 3;
+            print("Você possui: $vidas vidas\n\n");
         }
 
-    }while ($chute != $valorCorreto);
+        if($vidas == 0){
+            print("Você morreu!!");
+        }
+    }while ($chute != $valorCorreto && $vidas != 0);
+
+    print("\nTotal de pontos: $pontos");
 
